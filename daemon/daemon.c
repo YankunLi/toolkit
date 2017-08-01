570af1ae408f2c3db28502a5a73a06bedb36e5a9
YankunLi <lioveni99@gmail.com>
2017-08-01 18:56:25 +0800

    build daemon method

---
+++ daemon/daemon.c
@@ -0,0 +1,43 @@
+/*
+ *
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+int daemon(int no_ch_dir, int no_close_fd) {
+    //create child process
+    int pid = fork();
+
+    //exit parent process
+    if (pid < 0 | pid > 0) {
+        if (pid < 0)
+            return -1;
+        exit(0);
+    }
+
+    //create new session
+    if (setsid() < 0) {
+        return -1;
+    }
+
+    //change directory of work
+    if (0 == no_ch_dir) {
+        chdir("/");
+    }
+
+    //redirection for console
+    if (0 == no_close_fd) {
+        int fd = open("/dev/null", O_RDWR, 0);
+        if (fd < 0)
+            return -1;
+        dup2(fd, STDIN_FILENO);
+        dup2(fd, STDOUT_FILENO);
+        dup2(fd, STDERR_FILENO);
+    }
+
+    //handle signal
+    return 0;
+}
---
+++ daemon/daemon.h
@@ -0,0 +1,11 @@
+/*
+ *
+ *
+ */
+
+#ifndef _DAEMON_H_
+#define _DAEMON_H_
+
+int daemon(int no_ch_dir, int no_close_fd);
+
+#endif
