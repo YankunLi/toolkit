08cf53c8a4a03018424951cd9f238b345fb8bed4
YankunLi <lioveni99@gmail.com>
2017-08-02 17:49:50 +0800

    update daemon

--- daemon/daemon.c
+++ daemon/daemon.c
@@ -8,13 +8,19 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <signal.h>
+#include <sys/param.h>
 
 int init_daemon(int no_ch_dir, int no_close_fd) {
+
+    signal(SIGHUP ,SIG_IGN);
+    signal(SIGTTIN ,SIG_IGN);
+    signal(SIGTSTP ,SIG_IGN);
+    signal(SIGTTOU ,SIG_IGN);
     //create child process
     int pid = fork();
 
     //exit parent process
-    if (pid < 0 | pid > 0) {
+    if (pid < 0 || pid > 0) {
         if (pid < 0)
             return -1;
         exit(0);
@@ -24,6 +30,17 @@ int init_daemon(int no_ch_dir, int no_close_fd) {
     if (setsid() < 0) {
         return -1;
     }
+
+    pid = fork();
+    if (pid < 0 || pid > 0) {
+        if (pid < 0)
+            return -1;
+        exit(0);
+    }
+
+    int i = 0;
+    for (i = 0; i < NOFILE; i++)
+        close(i);
 
     //change directory of work
     if (0 == no_ch_dir) {
@@ -35,6 +52,7 @@ int init_daemon(int no_ch_dir, int no_close_fd) {
         int fd = open("/dev/null", O_RDWR, 0);
         if (fd < 0)
             return -1;
+
         dup2(fd, STDIN_FILENO);
         dup2(fd, STDOUT_FILENO);
         dup2(fd, STDERR_FILENO);
@@ -44,5 +62,6 @@ int init_daemon(int no_ch_dir, int no_close_fd) {
 
     //handle signal
     signal(SIGCHLD, SIG_IGN);
+
     return 0;
 }
